bd775aa77a0eff3d1bbe38c4a9d95e0f09a9e9e8
Benjamin De Letter <schildpad@dehavik.be>
2026-01-12 02:04:36 +0100

    feat: tabs at bottom for small screen (mobile) and at top for big screen (desktop)

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -1,20 +1,21 @@
 <template>
   <ion-page>
     <ion-tabs>
-      <ion-router-outlet></ion-router-outlet>
-      <ion-tab-bar slot="bottom">
+      <ion-router-outlet />
+
+      <ion-tab-bar :slot="tabSlot">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon aria-hidden="true" :icon="triangle" />
+          <ion-icon :icon="triangle" />
           <ion-label>Tab 1</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon aria-hidden="true" :icon="ellipse" />
+          <ion-icon :icon="ellipse" />
           <ion-label>Tab 2</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon aria-hidden="true" :icon="square" />
+          <ion-icon :icon="square" />
           <ion-label>Tab 3</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
@@ -23,6 +24,30 @@
 </template>
 
 <script setup lang="ts">
-import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
+import {
+  IonTabs,
+  IonTabBar,
+  IonTabButton,
+  IonLabel,
+  IonIcon,
+  IonPage,
+  IonRouterOutlet,
+} from '@ionic/vue';
 import { ellipse, square, triangle } from 'ionicons/icons';
+import { ref, onMounted, onUnmounted } from 'vue';
+
+const tabSlot = ref<'top' | 'bottom'>('bottom');
+
+const updateTabPosition = () => {
+  tabSlot.value = window.innerWidth > 1100 ? 'top' : 'bottom';
+};
+
+onMounted(() => {
+  updateTabPosition();
+  window.addEventListener('resize', updateTabPosition);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', updateTabPosition);
+});
 </script>
